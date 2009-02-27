7bac314889c9945cb8568737499ec9494c75eb7e
fiorix <fiorix@359170e0-0486-11de-994d-8308d8631604>
2009-02-27 04:42:57 +0000

    simple originate example
    git-svn-id: https://eventsocket.googlecode.com/svn/trunk@4 359170e0-0486-11de-994d-8308d8631604

--- example-simple-originate.py
+++ example-simple-originate.py
@@ -1,9 +1,25 @@
 #!/usr/bin/env python
 # coding: utf-8
+# freeswitch's event socket protocol for twisted
+# Copyright (C) 2009  Alexandre Fiori & Arnaldo Pereira
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 import os
-from twisted.internet import reactor, protocol
 from eventsocket import EventProtocol
+from twisted.internet import reactor, protocol
 
 class InboundProxy(EventProtocol):
     def authSuccess(self, ev):
@@ -43,7 +59,6 @@ class InboundFactory(protocol.ClientFactory):
     def clientConnectionFailed(self, connector, reason):
         print '[inboundfactoy] cannot connect: %s' % reason
         reactor.stop()
-
 
 if __name__ == '__main__':
     # outbound
