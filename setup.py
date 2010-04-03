9c8863421870dfb6fd8f82e16f08ba25a5e3816d
Alexandre Fiori <fiorix@gmail.com>
2010-04-03 19:24:18 -0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name="eventsocket",
+    version="0.1.3",
+    description="Twisted extension for the FreeSwitch Event Socket",
+    author="Alexandre Fiori",
+    url="http://github.com/fiorix/txeventsocket",
+    py_modules=["eventsocket"],
+    #install_requires=["twisted"],
+)
